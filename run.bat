45ab890f4f4adb8c03a3f60a6da26c12b7f3732c
james simon <swaggysimon24@gmail.com>
2026-03-24 09:53:39 -0700

    just /public

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0public"
-echo Starting ECITT Web App on http://localhost:8000
-echo.
-echo Press Ctrl+C to stop the server
-echo.
-set GOOGLE_SHEETS_DISABLE_SSL_VERIFY=1
-php -S 0.0.0.0:8000
-pause
